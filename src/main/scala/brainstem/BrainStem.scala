bb709a515e988e426f4b64f690d27f474b221221
Jared Stanbrough <jared.stanbrough@gmail.com>
2019-09-08 14:34:10 -0700

    PC inc dec

--- src/main/scala/brainstem/BrainStem.scala
+++ src/main/scala/brainstem/BrainStem.scala
@@ -1,10 +1,14 @@
 package brainstem
 
+import java.util.concurrent.atomic.AtomicInteger
+
 import spinal.core._
-import spinal.lib._
 import spinal.lib.fsm.{EntryPoint, State, StateMachine}
 
+import scala.collection.immutable
+import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
+
 
 class BrainStem extends Component {
   val io = new Bundle {
@@ -24,6 +28,80 @@ class BrainStem extends Component {
 
   val ledState = Reg(UInt(10 bits))
 
+  val code = new ArrayBuffer[BigInt]
+  val memSize = 128
+
+  /* BF to instruction bitmap:
+    < 0
+    > 1
+   */
+  for (_ <- 0 until memSize) {
+    val b0 = Random.nextBoolean()
+    if (b0) {
+      code.append(0)
+    } else {
+      code.append(1)
+    }
+  }
+
+  val memInit = {
+    for(i <- 0 until memSize) yield {
+      U(code(i), 8 bits)
+    }
+  }
+
+  val mem = new Mem(UInt(8 bits), memSize)
+  mem.init(memInit)
+
+  val pc = Reg(UInt(log2Up(memSize) bits))
+  val pcIncDec = Reg(Bool)
+  val pcIncEnable = Reg(Bool)
+
+  val op = Reg(UInt(8 bits))
+
+  val fsm = new StateMachine {
+    val Cold = new State with EntryPoint
+    val Fetch = new State
+    val Decode = new State
+
+    Cold.onEntry {
+      ledState := 0
+      pc := 0
+    }
+
+    Cold.whenIsActive {
+      goto(Fetch)
+    }
+
+    Fetch.onEntry {
+      op := mem(pc)
+      when(pcIncEnable) {
+        when(pcIncDec) {
+          pc := pc + 1
+        } otherwise {
+          pc := pc - 1
+        }
+      }
+      pcIncDec := False
+      pcIncEnable := False
+    }
+
+    Fetch.whenIsActive {
+       goto(Decode)
+    }
+
+    Decode.onEntry {
+      when(op === 0 || op === 1) {
+        pcIncDec := op === 1
+        pcIncEnable := True
+      }
+    }
+
+    Decode.whenIsActive {
+      goto(Fetch)
+    }
+  }
+
   io.LEDR_N := ledState(0)
   io.LEDG_N := ledState(1)
   io.LED_RED_N := ledState(2)
@@ -34,28 +112,11 @@ class BrainStem extends Component {
   io.LED3 := ledState(7)
   io.LED4 := ledState(8)
   io.LED5 := ledState(9)
-
-  val fsm = new StateMachine {
-    val Reset = new State with EntryPoint
-    val Cold = new State
-
-    Reset.onEntry {
-      ledState := 0
-    }
-
-    Reset.whenIsActive {
-        goto(Cold)
-    }
-
-    Cold.whenIsActive {
-      ledState := ledState + 1
-    }
-  }
 }
 
 object BrainStemVerilog {
   def main(args: Array[String]) {
-    SpinalVerilog(new BrainStem)
+    SpinalVerilog(new BrainStem).printPruned()
   }
 }
 
